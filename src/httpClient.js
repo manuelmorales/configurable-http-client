84e2b1b9b5cb072e072effde239ae7334c68c866
Manuel Morales <manuelmorales@gmail.com>
2018-02-03 09:18:53 +0100

    Fetch getsetters

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,1 +1,8 @@
-module.exports = {}
+module.exports = {
+  setFetch: function (fetch) { return Object.assign({}, this, {fetchAttribute: fetch}) },
+  getFetch: function () { return (this.fetchAttribute || global.fetch) },
+
+  get: function (url) {
+    this.getFetch()(url)
+  }
+}
--- src/httpClient.test.js
+++ src/httpClient.test.js
@@ -1,7 +1,38 @@
 const httpClient = require('./httpClient')
 
 describe('httpClient', () => {
-  it('exists', () => {
-    expect(httpClient).not.toBe(undefined)
+  describe('fetch', () => {
+    it('uses global.fetch by default', () => {
+      const fetch = jest.fn()
+      global.fetch = fetch
+
+      httpClient.get('https://example.com')
+
+      expect(fetch).toHaveBeenCalled()
+    })
+
+    it('allows setting a new fetch()', () => {
+      const globalFetch = jest.fn()
+      global.fetch = globalFetch
+
+      const fetch = jest.fn()
+      httpClient
+        .setFetch(fetch)
+        .get('https://example.com')
+
+      expect(fetch).toHaveBeenCalled()
+    })
+
+    it('doesnt\'t override the original one', () => {
+      const globalFetch = jest.fn()
+      global.fetch = globalFetch
+
+      const fetch = jest.fn()
+      httpClient.setFetch(fetch)
+
+      httpClient.get('https://example.com')
+
+      expect(globalFetch).toHaveBeenCalled()
+    })
   })
 })
