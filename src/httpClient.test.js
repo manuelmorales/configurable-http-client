5066d3a1fbccdf7f2f4968418f92814173792972
Manuel Morales <manuelmorales@gmail.com>
2018-05-02 17:49:54 +0200

    No more fat arrow functions in test for compatibility

--- src/httpClient.test.js
+++ src/httpClient.test.js
@@ -1,40 +1,40 @@
 const originalHttpClient = require('./httpClient')
 const fetchMock = require('fetch-mock')
 
-describe(`httpClient`, () => {
-  beforeEach(() => {
+describe(`httpClient`, function() {
+  beforeEach(function() {
     this.fetch = fetchMock.sandbox()
     this.fetch.get('/test', "Hello")
     this.fetch.get('/not_found', {status: 404, body: "Not Found"})
     this.httpClient = originalHttpClient.fetch(this.fetch)
-  })
+  }.bind(this))
 
-  describe(`fetch`, () => {
-    it(`uses global.fetch by default`, () => {
+  describe(`fetch`, function() {
+    it(`uses global.fetch by default`, function() {
       this.httpClient.runRequest('/test')
       expect(this.fetch.lastCall()).not.toBe(undefined)
-    })
+    }.bind(this))
 
-    it(`allows setting a new fetch()`, () => {
+    it(`allows setting a new fetch()`, function() {
       const newFetch = fetchMock.sandbox().get('/test2', "Hello")
       this.httpClient = this.httpClient.fetch(newFetch)
 
       this.httpClient.runRequest('/test2')
 
       expect(newFetch.lastCall()).not.toBe(undefined)
-    })
+    }.bind(this))
 
-    it(`doesn't override the original one`, () => {
+    it(`doesn't override the original one`, function() {
       const newFetch = fetchMock.sandbox().get('/test2', "Hello")
       this.httpClient = this.httpClient.fetch(newFetch)
 
       this.httpClient.runRequest('/test2')
 
       expect(this.fetch.lastCall()).toBe(undefined)
-    })
-  })
+    }.bind(this))
+  }.bind(this))
 
-  describe(`runRequest`, () => {
+  describe(`runRequest`, function() {
     it(`returns the response from fetch`, (done) => {
       this.httpClient.runRequest('/test')
 
@@ -45,7 +45,7 @@ describe(`httpClient`, () => {
 
         .catch(done)
     })
-  })
+  }.bind(this))
 
   it(`onResponse doesn't overwrite the original one`, (done) => {
     const oldSpy = jest.fn()
@@ -54,11 +54,11 @@ describe(`httpClient`, () => {
     const newSpy = jest.fn()
     const newClient = oldClient.onResponse(newSpy)
 
-    newClient.runRequest('/test').then(() => {
+    newClient.runRequest('/test').then(function() {
       expect(oldSpy).not.toHaveBeenCalled()
       expect(newSpy).toHaveBeenCalled()
       done()
-    })
+    }.bind(this))
   })
 
   it(`onSuccess doesn't overwrite the original one`, (done) => {
@@ -68,11 +68,11 @@ describe(`httpClient`, () => {
     const newSpy = jest.fn()
     const newClient = oldClient.onSuccess(newSpy)
 
-    newClient.runRequest('/test').then(() => {
+    newClient.runRequest('/test').then(function() {
       expect(oldSpy).not.toHaveBeenCalled()
       expect(newSpy).toHaveBeenCalled()
       done()
-    })
+    }.bind(this))
   })
 
   it(`onStatus doesn't overwrite the original one`, (done) => {
@@ -82,23 +82,23 @@ describe(`httpClient`, () => {
     const newSpy = jest.fn()
     const newClient = oldClient.onStatus(404, newSpy)
 
-    oldClient.runRequest('/not_found').then(() => {
+    oldClient.runRequest('/not_found').then(function() {
       expect(newSpy).not.toHaveBeenCalled()
       expect(oldSpy).toHaveBeenCalled()
       done()
-    })
+    }.bind(this))
   })
 
-  describe(`on 200`, () => {
+  describe(`on 200`, function() {
     it(`onResponse is called`, (done) => {
       const onResponse = jest.fn((resp) => { expect(resp.body).toEqual('Hello') })
 
       const newClient = this.httpClient.onResponse(onResponse)
 
-      newClient.runRequest('/test').then(() => {
+      newClient.runRequest('/test').then(function() {
         expect(onResponse).toHaveBeenCalled()
         done()
-      })
+      }.bind(this))
     })
 
     it(`gives precedence to onSuccess over onResponse`, (done) => {
@@ -111,25 +111,25 @@ describe(`httpClient`, () => {
             .onErrorResponse(onErrorResponse)
             .onSuccess(onSuccess)
 
-      newClient.runRequest('/test').then(() => {
+      newClient.runRequest('/test').then(function() {
         expect(onResponse).not.toHaveBeenCalled()
         expect(onErrorResponse).not.toHaveBeenCalled()
         expect(onSuccess).toHaveBeenCalled()
         done()
-      })
+      }.bind(this))
     })
-  })
+  }.bind(this))
 
-  describe(`on 400`, () => {
+  describe(`on 400`, function() {
     it(`onResponse is called`, (done) => {
       const onResponse = jest.fn((resp) => { expect(resp.body).toEqual('Not Found') })
 
       const newClient = this.httpClient.onResponse(onResponse)
 
-      newClient.runRequest('/not_found').then(() => {
+      newClient.runRequest('/not_found').then(function() {
         expect(onResponse).toHaveBeenCalled()
         done()
-      })
+      }.bind(this))
     })
 
     it(`gives precedence to onErrorResponse over onResponse`, (done) => {
@@ -142,12 +142,12 @@ describe(`httpClient`, () => {
             .onErrorResponse(onErrorResponse)
             .onSuccess(onSuccess)
 
-      newClient.runRequest('/not_found').then(() => {
+      newClient.runRequest('/not_found').then(function() {
         expect(onResponse).not.toHaveBeenCalled()
         expect(onSuccess).not.toHaveBeenCalled()
         expect(onErrorResponse).toHaveBeenCalled()
         done()
-      })
+      }.bind(this))
     })
 
     it(`gives precedence to onStatus(404) over onErrorResponse`, (done) => {
@@ -164,24 +164,24 @@ describe(`httpClient`, () => {
             .onStatus(404, on404)
             .onSuccess(onSuccess)
 
-      newClient.runRequest('/not_found').then(() => {
+      newClient.runRequest('/not_found').then(function() {
         expect(onResponse).not.toHaveBeenCalled()
         expect(onSuccess).not.toHaveBeenCalled()
         expect(onErrorResponse).not.toHaveBeenCalled()
         expect(on403).not.toHaveBeenCalled()
         expect(on404).toHaveBeenCalled()
         done()
-      })
+      }.bind(this))
     })
-  })
+  }.bind(this))
 
   it(`allows POST`, (done) => {
     this.fetch.post('/post', "Hello")
 
-    this.httpClient.runRequest('/post', { method: 'POST' }).then(() => {
+    this.httpClient.runRequest('/post', { method: 'POST' }).then(function() {
       expect(this.fetch.called('/post')).toBe(true)
       done()
-    })
+    }.bind(this))
   })
 
   it(`allows passing an object as json_body`, (done) => {
@@ -193,14 +193,14 @@ describe(`httpClient`, () => {
 
     const opts = { method: 'POST', json_body: {a: 1} }
 
-    this.httpClient.runRequest('/post', opts).then(() => {
+    this.httpClient.runRequest('/post', opts).then(function() {
       expect(this.fetch.called('/post')).toBe(true)
       done()
-    })
+    }.bind(this))
   })
 
   it('allows setting the request before the callbacks with request()', (done) => {
-    this.httpClient.request('/test').onStatus(200, () => done()).run()
+    this.httpClient.request('/test').onStatus(200, function() { done() }).run()
   })
 
   it('allows setting the request options before the callbacks', (done) => {
@@ -228,30 +228,30 @@ describe(`httpClient`, () => {
       .onStatus(200, on200)
       .onStatus(200, null)
 
-    newClient.runRequest('/test').then(() => {
+    newClient.runRequest('/test').then(function() {
       expect(on200).not.toHaveBeenCalled()
       expect(onSuccess).toHaveBeenCalled()
       done()
-    })
+    }.bind(this))
   })
 
-  describe('onConnectionError', () => {
-    it(`allows defining behavior on connection error`, () => {
+  describe('onConnectionError', function() {
+    it(`allows defining behavior on connection error`, function() {
       expect.assertions(1)
 
-      this.fetch.get('/connection_error', () => { throw 'Connection Error'})
+      this.fetch.get('/connection_error', function() { throw 'Connection Error'})
 
       const newClient = this.httpClient
         .onConnectionError((err) => { expect(err).toEqual('Connection Error') })
         .request('/connection_error')
 
       newClient.run()
-    })
+    }.bind(this))
 
     it(`doesn't catch errors in other callbacks`, (done) => {
       expect.assertions(1)
 
-      this.fetch.get('/connection_error', () => { throw 'Connection Error'})
+      this.fetch.get('/connection_error', function() { throw 'Connection Error'})
 
       const newClient = this.httpClient
         .onSuccess((resp) => { throw 'Expected error' })
@@ -267,7 +267,7 @@ describe(`httpClient`, () => {
     it(`lets the error go through as default behavior`, (done) => {
       expect.assertions(1)
 
-      this.fetch.get('/connection_error', () => { throw 'Connection Error'})
+      this.fetch.get('/connection_error', function() { throw 'Connection Error'})
 
       this.httpClient.runRequest('/connection_error').catch((err) => {
         expect(err).toEqual('Connection Error')
@@ -275,9 +275,9 @@ describe(`httpClient`, () => {
       })
     })
 
-    it.skip(`is respecful with the hyerarchy`, () => {})
-  })
+    it.skip(`is respecful with the hyerarchy`, function() {})
+  }.bind(this))
 
-  it.skip(`allows reading the parsed_body`, () => {})
-  it.skip(`allows passing query params as a parameter`, () => {})
+  it.skip(`allows reading the parsed_body`, function() {})
+  it.skip(`allows passing query params as a parameter`, function() {})
 })
